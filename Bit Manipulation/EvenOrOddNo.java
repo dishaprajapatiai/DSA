8fc40063105b4f4976092f2c1c97395427410f7e
disha9213 <dishap9213@gmail.com>
2026-07-02 13:33:19 +0530

    Check If number is even or odd

---
+++ Bit Manipulation/EvenOrOddNo.java
@@ -0,0 +1,12 @@
+public class EvenOrOddNo {
+    public static void main(String[] args) {
+        int n = 156;
+        int bitmask = 1;
+        int ans = n & bitmask;
+        if(ans == 0){
+            System.out.println("The number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+    }
+}
