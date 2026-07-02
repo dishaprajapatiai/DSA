844d270fa89368d36f0a450678f8ca62e091661c
disha9213 <dishap9213@gmail.com>
2026-07-02 17:32:59 +0530

    Set Bit

--- Bit Manipulation/Operations/setBit.java
+++ Bit Manipulation/Operations/setBit.java
@@ -2,7 +2,7 @@
 
 public class setBit {
     public static void main(String[] args) {
-        System.out.println(setIthBit(10, 2));
+        System.out.println(setIthBit(12, 2));
     }
 
     public static int setIthBit(int n ,int i){
