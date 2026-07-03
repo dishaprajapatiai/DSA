a8aa1acc6552275406c35c6b009bad957093bc52
disha9213 <dishap9213@gmail.com>
2026-07-03 17:13:05 +0530

    Ques 3

---
+++ Bit Manipulation/Questions/CountSetBit.java
@@ -0,0 +1,19 @@
+
+
+public class CountSetBit {
+    public static void main(String[] args) {
+        System.out.println(SetBit(10));
+    }
+
+    public static int SetBit(int n){
+        int count = 0;
+        while(n>0){
+            if((n&1) != 0){
+                count ++;
+            }
+            n =  n >> 1;
+            
+        }
+        return count;
+    }
+}
