5ba6116d9808239111a9ba1aef2cb48032142426
disha9213 <dishap9213@gmail.com>
2026-07-03 12:02:24 +0530

    Question1

---
+++ Bit Manipulation/Questions/PowerOf2.java
@@ -0,0 +1,10 @@
+// To check whether a number is the power of 2 or not 
+public class PowerOf2 {
+    public static void main(String[] args) {
+        System.out.println(Power(8));
+    }
+
+    public static boolean Power(int n){
+        return (n &(n-1)) == 0; // checks the number and one smaller number to it and AND between them will give 0
+    }
+}
