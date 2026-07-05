4132af9e78d9f3cf1384785181b805e4c1138eda
disha9213 <dishap9213@gmail.com>
2026-07-05 17:24:13 +0530

    Added a file

--- OOP/Intro.java
+++ OOP/Intro.java
@@ -1,3 +1,17 @@
 public class Intro {
     
 }
+
+class Pen{
+    // prop + func
+    String color;
+    int tip;
+
+    void setColor(newColor){
+        color = newColor;
+    }
+
+    void setTip(newTip){
+        
+    }
+}
